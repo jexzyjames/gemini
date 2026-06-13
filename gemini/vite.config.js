fe58e60f3795679216d5e81deb1ebcbdd58bc24d
James Jegede <jexzyjames@gmail.com>
2026-06-13 02:40:01 +0100

    Load environment variables in Vite config

--- gemini/vite.config.js
+++ gemini/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+Import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
+export default defineConfig (({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define:{
+      'process.env':env
+      
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    },
+    plugins: [react(), tailwindcss()],
+  }
 })
