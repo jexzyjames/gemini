95d2bca2db860c1a77097cbf7c7085afcd44d320
James Jegede <jexzyjames@gmail.com>
2026-06-13 19:16:43 +0100

    fixed the toastcontainer issue that stops rendering white screen

--- gemini/src/App.jsx
+++ gemini/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import Main from './components/Main'
-import { ToastContainer, toast } from "react-toastify";
-
+import { ToastContainer } from "react-toastify";
+// Make sure you have the styles imported so toasts look correct!
+import 'react-toastify/dist/ReactToastify.css'; 
 
 function App() {
- 
   return (
-    <div className='w-full m-0 '>
-    <ToastContainer>
-    <Main  />
-    </ToastContainer>
+    <div className='w-full m-0'>
+      {/* FIXED: Main is now placed normally on the page */}
+      <Main />
+      
+      {/* FIXED: ToastContainer is now self-closing */}
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   )
 }
