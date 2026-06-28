5e692ccecd08d03f7c5c25e9794f028524531cff
James Jegede <jexzyjames@gmail.com>
2026-06-28 09:57:31 +0100

    Remove environment variable handling in vite.config.js
    Removed environment variable loading from Vite config.

--- gemini/vite.config.js
+++ gemini/vite.config.js
@@ -4,13 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react(), tailwindcss()],
-    define: {
-      // Stringify and pass the loaded environment variables to the client
-      'process.env': env
-    }
   }
 })
 
